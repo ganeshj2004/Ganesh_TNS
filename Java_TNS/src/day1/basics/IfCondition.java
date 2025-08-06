bf19d06a024bf940a5b967eae521e7775aa08bc9
ganeshj2004 <jaisankarganesh156@gmail.com>
2025-08-06 23:42:23 +0530

    second commit

---
+++ Java_TNS/src/day1/basics/IfCondition.java
@@ -0,0 +1,17 @@
+package day1.basics;
+
+public class IfCondition {
+
+	public static void main(String[] args) {
+		int x = 6;
+		if (x != 5) {{
+			System.out.println("Value of x is not 5");
+			}}
+
+		else {
+		System.out.println("Value of x is 5");
+	}
+
+	}
+
+}
